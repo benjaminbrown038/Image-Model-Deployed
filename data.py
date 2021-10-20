cd7ed136e7da97071fdad68c8410ee230ffbe5c4
Benjamin Brown <35114142+benjaminbrown038@users.noreply.github.com>
2021-10-20 10:05:59 -0400

    last update with comments

--- data.py
+++ data.py
@@ -1,16 +1,28 @@
+# pause after scrolling results page 
 import time 
+# for accessing web 
 import selenium 
+# for chromedriver
 from selenium import webdriver
+# to navigate results page 
 from selenium.webdriver.common.keys import Keys
+# to retreive http data
 import requests
+# checking and making directory
 import os
+# open image after decoded
 import PIL
+# open image after decoded
 from PIL import Image
+# decoding 
 import io
+# decoding 
 import base64 
 
+# create class called Data
 class Data():    
     
+    # initializer for class with input search name 
     def __init__(self,search_name):
         # specific to user Desktop where chrome driver is downloaded
         DRIVER_PATH = '/home/trey/Sharpest-Minds-Project/chromedriver'
@@ -46,19 +58,26 @@ class Data():
                         new_edit = new + '=='
                         # image becomes Image object
                         new_image = (Image.open(io.BytesIO(base64.b64decode(new_edit)))).resize((150,150))
+                        # append image to list
                         images.append(new_image)
                     else:
+                        # open image as Image object
                         new_image = (Image.open(io.BytesIO(base64.b64decode(new)))).resize((150,150))
+                        # append image to list 
                         images.append(new_image)
                 if image[0:4] == 'http':
+                    # http url from web results 
                     new = requests.get(image)
+                    # decode and create image as Image object
                     new_image = Image.open(io.BytesIO(new.content))
+                    # save to list 
                     images.append(new_image)
-        # creating directories, saving images, and creating names for images as jpeg files
+        # creating directories for training and testing 
         os.makedirs('Images/', exist_ok=True)
         os.makedirs('Images/training', exist_ok=True)
         os.makedirs('Images/testing', exist_ok=True)
         index = 0 
+        # iterating through list where images are saved and saving images as jpeg to just created directories
         for i in images:
             i.save('Images/training'+'/'+ search_name + str(index) +'.jpeg')
             index += 1 

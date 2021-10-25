53408c153a78c57e10167fadffa088076633aaa2
benjaminbrown038 <benjaminbrown038@github.com>
2021-10-25 10:07:02 -0400

    first commit: documentation

--- data.py
+++ data.py
@@ -1,10 +1,14 @@
-# pause after scrolling results page 
-import time 
-# for accessing web 
-import selenium 
+'''
+how to document imports 
+
+'''
+# pause after scrolling results page
+import time
+# for accessing web
+import selenium
 # for chromedriver
 from selenium import webdriver
-# to navigate results page 
+# to navigate results page
 from selenium.webdriver.common.keys import Keys
 # to retreive http data
 import requests
@@ -14,15 +18,15 @@ import os
 import PIL
 # open image after decoded
 from PIL import Image
-# decoding 
+# decoding
 import io
-# decoding 
-import base64 
+# decoding
+import base64
 
 # create class called Data
-class Data():    
-    
-    # initializer for class with input search name 
+class Data():
+
+    # initializer for class with input search name
     def __init__(self,search_name):
         # specific to user Desktop where chrome driver is downloaded
         PATH = os.getcwd()
@@ -31,10 +35,10 @@ class Data():
         # specific for requesting images
         search_url = "https://www.google.com/search?q={q}&sxsrf=ALeKk02zAb9RaNNb-qSenTEJh1i2XX480w:1613489053802&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjChJqP2-7uAhVyTTABHdX0CPoQ_AUoAXoECAcQAw&biw=767&bih=841"
         self.wd.get(search_url.format(q=search_name))
-    
-    # get images links, remove noise, decode cleaned data, size and open image, save image, create folder, save images from search to folder 
+
+    # get images links, remove noise, decode cleaned data, size and open image, save image, create folder, save images from search to folder
     def scrape_and_save(self,search_name):
-        
+
         body = self.wd.find_element_by_tag_name("body")
         # scrolling search page results
         body.send_keys(Keys.PAGE_DOWN)
@@ -43,14 +47,14 @@ class Data():
         main = self.wd.find_elements_by_class_name("rg_i.Q4LuWd")
         # getting image links (ASCII data communication) stored as base64 and http urls from img (html) tag with src holding the path (url)
         links = [main[i].get_attribute('src') for i in range(len(main))]
-        # shut down web page 
+        # shut down web page
         self.wd.quit()
         images = []
         #
         for image in links:
             # going through image links which stored as strings
             if type(image) == str:
-                # checking base64 text 
+                # checking base64 text
                 if image[0:4] == 'data':
                     # remove noise (cleaning data)
                     new = image.replace("data:image/jpeg;base64,","")
@@ -64,24 +68,24 @@ class Data():
                     else:
                         # open image as Image object
                         new_image = (Image.open(io.BytesIO(base64.b64decode(new)))).resize((150,150))
-                        # append image to list 
+                        # append image to list
                         images.append(new_image)
                 if image[0:4] == 'http':
-                    # http url from web results 
+                    # http url from web results
                     new = requests.get(image)
                     # decode and create image as Image object
                     new_image = Image.open(io.BytesIO(new.content))
-                    # save to list 
+                    # save to list
                     images.append(new_image)
-        # creating directories for training and testing 
+        # creating directories for training and testing
         # data is saved to the folder that is created according to each instance of Data, exist_ok True for multiple instances of Data
         os.makedirs('Images/', exist_ok=True)
         # exists_ok True if same search_name is ran more than once, the folder will stay
         os.makedirs('Images/'+ search_name, exist_ok=True)
-        index = 0 
+        index = 0
         # iterating through list where images are saved and saving images as jpeg to just created directories
-        #split_folders.ratio('Images', output="output", seed=1337, ratio=((.8, 0.2))) 
-        index = 0 
+        #split_folders.ratio('Images', output="output", seed=1337, ratio=((.8, 0.2)))
+        index = 0
         for i in images:
             # saving images according to search_name in search_name directory
             i.save('Images/'+ search_name + '/' + str(index) + '.jpeg')

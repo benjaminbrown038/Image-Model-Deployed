26860dca501b86dbaccd2ebd1171bd62d39d42dc
Benjamin Brown <35114142+benjaminbrown038@users.noreply.github.com>
2022-05-12 17:34:07 -0600

    Add files via upload

--- data.py
+++ data.py
@@ -1,5 +1,19 @@
+import splitfolders
+# test, train, val
+splitfolders.ratio("Images", output="Augmented",seed=1337, ratio=(.8, .1, .1), group_prefix=None, move=False)
 # augment
-
-# build model
-
-# train
+import torchvision.transforms as T
+# training augmentation types: horizontal flip, rotation, crop, normalize
+# 12 augmentation strategies
+train_transform = T.Compose(T.GaussianBlur(),
+                        T.ColorJitter(),
+                        T.RandomHorizontalFLip(),
+                        T.RandomRotation(),
+                        T.RandomVerticalFlip(),
+                        T.GaussianBlur(),
+                        T.LinearTransformation(),
+                        T.adjust_brightness(),
+                        T.adjust_contrast(),
+                        T.adjust_hue(),
+                        T.adjust_saturation(),
+                        T.adjust_sharpness())

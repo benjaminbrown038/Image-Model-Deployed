e13978b2d9629e5a63956bc69adf2c03fc41cee9
Benjamin Brown <35114142+benjaminbrown038@users.noreply.github.com>
2022-05-17 21:57:00 -0400

    Update data.py

--- data.py
+++ data.py
@@ -1,6 +1,7 @@
 import splitfolders
 # test, train, val
 splitfolders.ratio("Images", output="Augmented",seed=1337, ratio=(.8, .1, .1), group_prefix=None, move=False)
+#https://pytorch.org/vision/stable/index.html
 # augment
 import torchvision.transforms as T
 # training augmentation types: horizontal flip, rotation, crop, normalize
@@ -17,3 +18,16 @@ train_transform = T.Compose(T.GaussianBlur(),
                         T.adjust_hue(),
                         T.adjust_saturation(),
                         T.adjust_sharpness())
+
+
+import torch
+from torch import sequential
+from torch.nn import conv2d, MaxPool2d
+input_shape = (150,150)
+model = sequential(
+        conv2d(8,16, (3,3),(2,2),(1,1)),
+        maxpool2d((3,3),(2,2)),
+        conv2d(16,32,(3,3),(2,2),(1,1)),
+        maxpool2d((3,3),(2,2)),
+        conv2d(32,64,),
+        maxpool2d())

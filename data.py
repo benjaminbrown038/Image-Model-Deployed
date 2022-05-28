2e9875cee5399bd995af57739cdd5753f87330e9
Benjamin Brown <35114142+benjaminbrown038@users.noreply.github.com>
2022-05-28 16:39:47 -0400

    Update data.py

--- data.py
+++ data.py
@@ -1,44 +1,20 @@
 import splitfolders
-# test, train, val
-splitfolders.ratio("Images", output="Augmented",seed=1337, ratio=(.8, .1, .1), group_prefix=None, move=False)
-#https://pytorch.org/vision/stable/index.html
-# data 
-import torchvision
-train = torchvision.datasets.CIFAR10(root='./data', train=True,download=True, transform=transform)
-
-trainloader = torch.utils.data.DataLoader(train, batch_size=batch_size,shuffle=True, num_workers=2)
-
-test = torchvision.datasets.CIFAR10(root='/data',test = True, download = True, transform = transform)
-
-test_loader = torch.utils.data.DataLoader(test, batch_size = batch_size, shuffle = True, num_workers = 2)
-
-classes = ('plane','car','bird','cat','deer','dog','frog','horse','ship','truck')
-
-# augment
-import torchvision.transforms as T
-# training augmentation types: horizontal flip, rotation, crop, normalize
-# 12 augmentation strategies
-train_transform = T.Compose(T.GaussianBlur((3,3)),
-                        T.ColorJitter(),
-                        T.RandomHorizontalFLip(0.5),
-                        T.RandomRotation(0.5),
-                        T.RandomVerticalFlip(0.5),
-                        T.GaussianBlur((3,3),
-                        T.LinearTransformation(),
-                        T.adjust_brightness(),
-                        T.adjust_contrast(),
-                        T.adjust_hue(),
-                        T.adjust_saturation(),
-                        T.adjust_sharpness())
-
-
 import torch
+import torchvision
 from torch import sequential
-from torch.nn import conv2d, MaxPool2d
-from torch.nn import functional
+from torch.nn import conv2d, MaxPool2d, functional
+from torch import optim
+
+# data 
+train = torchvision.datasets.CIFAR10(root='./data', train=True,download=True, transform=transform)
+trainloader = torch.utils.data.DataLoader(train, batch_size=batch_size,shuffle=True, num_workers=2)
+# tess
+test = torchvision.datasets.CIFAR10(root='/data',test = True, download = True, transform = transform)
+test_loader = torch.utils.data.DataLoader(test, batch_size = batch_size, shuffle = True, num_workers = 2)
+#classes 
+classes = ('plane','car','bird','cat','deer','dog','frog','horse','ship','truck')
                             
-input_shape = (150,150)
-                            
+input_shape = (150,150)                          
 model = nn.Sequential(
             
             nn.Conv2d(3, 32, kernel_size = 3, padding = 1),
@@ -66,3 +42,7 @@ model = nn.Sequential(
             nn.ReLU(),
             nn.Linear(512,10)
         )
+
+optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+loss = nn.CrossEntropyLoss()
+

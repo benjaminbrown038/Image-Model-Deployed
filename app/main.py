ce0d899fa4dc833731d851db7bd2ad164558dfd2
scientistkev <kpmcpherson@gmail.com>
2022-06-07 12:58:07 -0500

    adds instructions to main.py for API building

--- app/main.py
+++ app/main.py
@@ -1,1 +1,7 @@
 # Can fill in here or migrate other files
+
+# Check out the tutorial on FastAPI for details: https://fastapi.tiangolo.com/tutorial/
+
+# Ideally, we want 1 route in our API (maybe call it /predict/ or something)
+# We also want to create a class that defines our input and the data types that come with that class. 
+# The rest is really up to you. But these are good practices to have when creating an API

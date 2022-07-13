34a98ff061487c3875a89c0bea494c4c2ea0cbf4
Benjamin Brown <35114142+benjaminbrown038@users.noreply.github.com>
2022-07-13 11:15:22 -0600

    Update main.py

--- app/main.py
+++ app/main.py
@@ -8,7 +8,8 @@ from fastapi import FastAPI, File, UploadFile
 # Check out the tutorial on FastAPI for details: https://fastapi.tiangolo.com/tutorial/
 
 # Ideally, we want 1 route in our API (maybe call it /predict/ or something)
-# We also want to create a class that defines our input and the data types that come with that class. 
+# We also want to create a:
+# 'Class user()' that defines our input and the data types that come with that class. 
 # The rest is really up to you. But these are good practices to have when creating an API
 
 app = FastAPI()
